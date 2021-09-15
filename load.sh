521adeaa4afe5c34270372a7c1df31a79b71570d
antony-wong <78070933+antony-wong@users.noreply.github.com>
2021-09-15 12:07:01 +0800

    Create load.sh

---
+++ load.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/antony-wong/test-node/main/cert.pem
+wget https://raw.githubusercontent.com/antony-wong/test-node/main/key.pem
+wget https://raw.githubusercontent.com/antony-wong/test-node/main/app.js
+
+node app.js
